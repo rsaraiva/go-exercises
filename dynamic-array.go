146b0dff89b19c2345b3012dab37f2fe2377bf17
Rubens Saraiva <rubens.saraiva@gmail.com>
2019-11-23 15:25:10 -0300

    implementing dynamic-array algorithm

---
+++ dynamic-array.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+func dynamicArray(n int32, queries [][]int32) []int32 {
+
+	var seqList [][]int32
+	for i := 0; i < int(n); i++ {
+		seqList = append(seqList, []int32{})
+	}
+
+	var lastAnswer int32 = 0
+
+	var ret []int32
+
+	for k, _ := range queries {
+
+		t := queries[k][0]
+		x := queries[k][1]
+		y := queries[k][2]
+
+		index := (x ^ lastAnswer) % n
+
+		if t == 1 {
+			seqList[index] = append(seqList[index], y)
+		} else {
+			seq := seqList[index]
+			size := len(seq)
+			index_ := y % int32(size)
+			lastAnswer = seq[index_]
+
+			ret = append(ret, lastAnswer)
+		}
+	}
+
+	return ret
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	qTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	q := int32(qTemp)
+
+	var queries [][]int32
+	for i := 0; i < int(q); i++ {
+		queriesRowTemp := strings.Split(strings.TrimRight(readLine(reader), " \t\r\n"), " ")
+
+		var queriesRow []int32
+		for _, queriesRowItem := range queriesRowTemp {
+			queriesItemTemp, err := strconv.ParseInt(queriesRowItem, 10, 64)
+			checkError(err)
+			queriesItem := int32(queriesItemTemp)
+			queriesRow = append(queriesRow, queriesItem)
+		}
+
+		if len(queriesRow) != 3 {
+			panic("Bad input")
+		}
+
+		queries = append(queries, queriesRow)
+	}
+
+	result := dynamicArray(n, queries)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	//fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
